0f2ac3d8bedcaff7eba8bf95d9e64179cfdf3e44
tpqnjenny <tpqnjenny@naver.com>
2025-04-14 21:29:13 +0900

    20250414 dbconn

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,9 @@
 <?php
-  $connect = mysqli_connect("localhost", "tpqnjenny97", "rladldud97!", 'join_nesthotel')
-  or ("SQL server에 연결할 수 없습니다.");
-  mysqli_select_db($connect, "tpqnjenny97");
+
+$connect = mysqli_connect("localhost", "tpqnjenny97", "rladldud97!", 'join_nesthotel');
+
+if (!$connect) {
+  die("SQL server에 연결할 수 없습니다: " . mysqli_connect_error());
+}
   
 ?>
